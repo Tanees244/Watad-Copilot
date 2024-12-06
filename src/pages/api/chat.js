58f557dc325b97a339667dbe132067c83822cad0
Abdul Ahad <abdulahad.available@gmail.com>
2024-12-06 11:23:32 +0500

    api issues resolved

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -2,7 +2,7 @@ import connectMongoDB from '../../utils/dbConnect';
 import Order from '../../models/Order';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
-const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
